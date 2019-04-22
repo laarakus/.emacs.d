e379027ca87d6422620d6df758936112df669654
Walid LAARAKI <wlaaraki@maisonsdumonde.com>
2019-04-22 18:04:22 +0200

    adding new packages

--- custom.el
+++ custom.el
@@ -4,6 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(emms-browser-default-browse-type (quote info-album))
+ '(global-git-gutter-mode t)
+ '(global-whitespace-mode t)
  '(helm-always-two-windows nil)
  '(helm-full-frame nil)
  '(helm-migemo-mode nil)
@@ -18,7 +20,7 @@
  '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (helm-ag flycheck kubernetes magit exec-path-from-shell avy ivy-rich flx counsel ivy restclient coffee-mode lorem-ipsum apache-mode itail logview impatient-mode skewer-mode nginx-mode web-beautify js2-mode web-mode w3m php-mode gnuplot helm-emms emms bbdb-vcard bbdb rainbow-delimiters beacon zenburn-theme use-package undo-tree rainbow-mode org-plus-contrib helm-swoop helm-projectile helm-descbinds helm-company hc-zenburn-theme company-web company-restclient)))
+    (diff-hl git-gutter which-key helm-ag flycheck kubernetes magit exec-path-from-shell avy ivy-rich flx counsel ivy restclient coffee-mode lorem-ipsum apache-mode itail logview impatient-mode skewer-mode nginx-mode web-beautify js2-mode web-mode w3m php-mode gnuplot helm-emms emms bbdb-vcard bbdb rainbow-delimiters beacon zenburn-theme use-package undo-tree rainbow-mode org-plus-contrib helm-swoop helm-projectile helm-descbinds helm-company hc-zenburn-theme company-web company-restclient)))
  '(safe-local-variable-values (quote ((eval org-columns))))
  '(show-trailing-whitespace t)
  '(standard-indent 2))
@@ -27,5 +29,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(git-gutter:added ((t (:foreground "#7F9F7F" :inverse-video t))))
+ '(git-gutter:deleted ((t (:foreground "#CC9393"))))
+ '(hl-line ((t nil)))
  '(ivy-current-match ((t (:background "gray21" :foreground "#F0DFAF" :underline nil :weight bold))))
- '(org-level-1 ((t (:foreground "#DFAF8F" :height 1.0)))))
+ '(org-level-1 ((t (:foreground "#DFAF8F" :height 1.0))))
+ '(whitespace-empty ((t (:background "#3F3F3F"))))
+ '(whitespace-hspace ((t (:foreground "#4F4F4F"))))
+ '(whitespace-indentation ((t (:background "#3F3F3F" :foreground "#4F4F4F"))))
+ '(whitespace-space ((t (:background "#3F3F3F" :foreground "#4F4F4F"))))
+ '(whitespace-space-after-tab ((t (:background "#3F3F3F" :foreground "#4F4F4F")))))
