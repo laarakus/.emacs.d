c01c3d0bececdc38cd79f77575cc4ba189d0e0a4
Walid Laaraki <walid.laaraki@ontexglobal.com>
2021-08-24 11:17:24 +0200

    change fill column

--- custom.el
+++ custom.el
@@ -3,25 +3,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(emms-browser-default-browse-type (quote info-album))
+ '(emms-browser-default-browse-type 'info-album)
  '(global-git-gutter-mode t)
  '(global-whitespace-mode t)
  '(helm-always-two-windows nil)
  '(helm-full-frame nil)
  '(helm-migemo-mode nil)
  '(helm-swoop-move-to-line-cycle nil)
- '(helm-swoop-pre-input-function (quote #[0 "\300\301!\207" [thing-at-point symbol] 2]))
+ '(helm-swoop-pre-input-function '#[0 "\300\301!\207" [thing-at-point symbol] 2])
  '(helm-swoop-speed-or-color t)
- '(helm-swoop-split-direction (quote split-window-vertically))
+ '(helm-swoop-split-direction 'split-window-vertically)
  '(helm-swoop-split-with-multiple-windows nil)
  '(helm-swoop-use-fuzzy-match t)
  '(helm-swoop-use-line-number-face t)
  '(indent-tabs-mode t)
  '(org-agenda-files nil)
  '(package-selected-packages
-   (quote
-    (prettier-js prettier-emacs dash helm-lsp lsp-ui lsp-mode git-timemachine diff-hl git-gutter which-key helm-ag flycheck kubernetes magit exec-path-from-shell avy ivy-rich flx counsel ivy restclient coffee-mode lorem-ipsum apache-mode itail logview impatient-mode skewer-mode nginx-mode web-beautify js2-mode web-mode w3m php-mode gnuplot helm-emms emms bbdb-vcard bbdb rainbow-delimiters beacon zenburn-theme use-package undo-tree rainbow-mode org-plus-contrib helm-swoop helm-projectile helm-descbinds helm-company hc-zenburn-theme company-web company-restclient)))
- '(safe-local-variable-values (quote ((eval org-columns))))
+   '(prettier prettier-js prettier-emacs dash helm-lsp lsp-ui lsp-mode git-timemachine diff-hl git-gutter which-key helm-ag flycheck kubernetes magit exec-path-from-shell avy ivy-rich flx counsel ivy restclient coffee-mode lorem-ipsum apache-mode itail logview impatient-mode skewer-mode nginx-mode web-beautify js2-mode web-mode w3m php-mode gnuplot helm-emms emms bbdb-vcard bbdb rainbow-delimiters beacon zenburn-theme use-package undo-tree rainbow-mode org-plus-contrib helm-swoop helm-projectile helm-descbinds helm-company hc-zenburn-theme company-web company-restclient))
+ '(safe-local-variable-values '((eval org-columns)))
  '(show-trailing-whitespace t)
  '(standard-indent 2))
 (custom-set-faces
